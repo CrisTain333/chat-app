d70f6283f5f6c981da236e9305671da7d12c62ad
Sukanta Das <arnobh84@gmail.com>
2023-07-17 15:45:53 +0600

    channge

--- client/src/router/index.tsx
+++ client/src/router/index.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/home/Home";
 import Chat from "../pages/chat/Chat";
 
-const router = createBrowserRouter([
+export const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
