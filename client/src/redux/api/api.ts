610e889b1e2b3a317e6f3a2587eb73da39b7de71
Sukanta Das <arnobh84@gmail.com>
2023-07-22 15:22:22 +0600

    redux store setup and root api added

---
+++ client/src/redux/api/api.ts
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+// Need to use the React-specific entry point to import createApi
+import {
+  createApi,
+  fetchBaseQuery,
+} from "@reduxjs/toolkit/query/react";
+import { RootState } from "..";
+
+// Define a service using a base URL and expected endpoints
+export const api = createApi({
+  reducerPath: "api",
+  tagTypes: ["book", "singleBook"],
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:8000/api/v1",
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).auth.token;
+      headers.set("authorization", token);
+      return headers;
+    },
+  }),
+  endpoints: () => ({}),
+});
+
+// // Export hooks for usage in functional components, which are
+// // auto-generated based on the defined endpoints
+// export const { useGetPokemonByNameQuery } = pokemonApi;
---
+++ client/src/redux/index.ts
@@ -0,0 +1,18 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { api } from "./api/api";
+import authReducer from "./feature/user/userSlice";
+export const store: any = configureStore({
+  reducer: {
+    auth: authReducer,
+    // book: bookReducer,
+    // wishlist: wishlistReducer,
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
