7c28375c505fc85fac6d89bd0ee3962dee8f99f2
Sukanta Das <arnobh84@gmail.com>
2023-07-22 15:40:53 +0600

    them added for login page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import { router } from "./router";
 
 const App = () => {
   return (
-    <div>
+    <div className="">
       <RouterProvider router={router}></RouterProvider>
     </div>
   );
--- client/src/pages/auth/signin.tsx
+++ client/src/pages/auth/signin.tsx
@@ -1,7 +1,116 @@
+import { Box, Circle } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
 const SignIn = () => {
+  const dispatch = useAppDispatch();
+  const [loginUser, { isLoading }] = useLoginUserMutation();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
+  const navigate = useNavigate();
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    const data = {
+      email,
+      password,
+    };
+    const response: any = await loginUser(data);
+    const { data: responseData, error } = response;
+
+    if (responseData?.statusCode === 200) {
+      toast.success(responseData?.message);
+      await dispatch(
+        getUser(responseData?.data?.accessToken)
+      );
+      dispatch(setToken(responseData?.data?.accessToken));
+      navigate(from, { replace: true });
+    } else {
+      toast.error(error?.data?.message);
+    }
+  };
   return (
     <div>
-      <p>Sign in Page</p>
+      <Box
+        position={"relative"}
+        bg={"#100E27"}
+        height={"100vh"}
+      >
+        <Circle
+          position={"absolute"}
+          zIndex={"1"}
+          left={"13%"}
+          top={"-730px"}
+          filter={"blur(120px)"}
+          size={"1000px"}
+          bg="#2655D4"
+        />
+        <main className="w-full h-screen flex flex-col items-center justify-center px-4 ">
+          <div className="max-w-md p-2 w-full text-gray-600 ">
+            <div className="text-center">
+              {/* <div className="flex items-center justify-center">
+                <img
+                  src={logo}
+                  width={90}
+                  // className="mx-auto"
+                />
+                <span className="text-3xl text-black font-semibold -ml-5">
+                  ookVerse
+                </span>
+              </div> */}
+              <div className="mt-5 space-y-2">
+                <h3 className="text-gray-800 text-2xl font-bold sm:text-3xl">
+                  Log in to your account
+                </h3>
+              </div>
+            </div>
+            <form
+              onSubmit={handleSubmit}
+              className="mt-8 space-y-5"
+            >
+              <div>
+                <label className="font-medium">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  required
+                  className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border  shadow-sm rounded-lg"
+                />
+              </div>
+              <div>
+                <label className="font-medium">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  required
+                  className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border  shadow-sm rounded-lg"
+                />
+              </div>
+              <button
+                className="w-full px-4 py-2 text-white font-medium  rounded-md duration-150 bg-[#f62343]"
+                type="submit"
+              >
+                {isLoading ? <SmallLoader /> : <>Login </>}
+              </button>
+            </form>
+            <div className="mt-5 text-center">
+              <p className="">
+                Don't have an account?{" "}
+                <Link
+                  to="/auth/signup"
+                  className="font-medium text-indigo-600 hover:text-indigo-500"
+                >
+                  Sign up
+                </Link>
+              </p>
+            </div>
+          </div>
+        </main>
+      </Box>
     </div>
   );
 };
