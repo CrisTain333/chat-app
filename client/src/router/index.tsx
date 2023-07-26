333e08e6b7287c5647391ccc86231c2ecd28ada4
Sukanta Das <arnobh84@gmail.com>
2023-07-26 12:33:45 +0600

    favicon added

--- client/src/router/index.tsx
+++ client/src/router/index.tsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter } from "react-router-dom";
-import Home from "../pages/home/Home";
 import Chat from "../pages/chat/Chat";
 import SignIn from "../pages/auth/signin";
 import SignUp from "../pages/auth/signup";
